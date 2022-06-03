9040a7fbcf5f7777db307e500cc30cb33e21e54b
Roberto Redradix <roberto@redradix.com>
2022-06-03 14:42:20 +0200

    Refactor Aprox03

--- web/components/responsive-image/Aprox03.jsx
+++ web/components/responsive-image/Aprox03.jsx
@@ -1,17 +1,25 @@
+const API_ORIGIN = process.env.NEXT_PUBLIC_API_ORIGIN
+
 const Aprox03 = ({ image, ...props }) => {
-  const API_ORIGIN = process.env.NEXT_PUBLIC_API_ORIGIN
-  const formats = Object.keys(image.formats)
-    .map(
-      (format) =>
-        `${API_ORIGIN}${image.formats[format].url} ${image.formats[format].width}w`,
-    )
-    .concat(`${API_ORIGIN}${image.url} ${image.width}w`)
-    .join()
+  const imageCandidates = [
+    /* IMPORTANT Include the original image in case there are no formats
+     * available or the viewport supports its size just fine.
+     */
+    image,
+    ...Object.values(image.formats),
+  ]
+    .map((strapiFormat) => {
+      const absoluteUrl = API_ORIGIN.concat(strapiFormat.url)
+      const widthDescriptor = `${strapiFormat.width}w`
+
+      return `${absoluteUrl} ${widthDescriptor}`
+    })
+    .join(', ')
 
   return (
     <img
       loading="lazy"
-      srcSet={formats}
+      srcSet={imageCandidates}
       width={image.width}
       height={image.height}
       alt={image.alternativeText}
